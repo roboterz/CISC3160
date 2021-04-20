a090036e69e1bd21ba303cf4fe371249eec4997e
roboterz <40050902+roboterz@users.noreply.github.com>
2021-04-20 02:06:09 -0400

    Update Lab4_1.asm

--- Lab4/Lab4_1.asm
+++ Lab4/Lab4_1.asm
@@ -6,7 +6,7 @@ main:
 	lw $t1, X	# let t1 = X
 	li $t0, 10	# let t0 = 10
 Loop:
-	bge $t1, $t0, Exit	# if X(t1) great or equal to 10(t1), jump to label "Exit".
+	bge $t1, $t0, Exit	# if X(t1) great or equal to 10(t0), jump to label "Exit".
 	addi $t1, $t1, 1	# X++
 	j Loop			# go back to label "Loop"
 Exit:
